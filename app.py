e21c0d48241f33139a0b80b17b28c6207350d08d
alihassan <24819103+thisalihassan@users.noreply.github.com>
2020-06-24 00:10:51 +0500

    cors issue

--- app.py
+++ app.py
@@ -10,21 +10,20 @@ from PIL import Image
 from io import BytesIO
 
 app = Flask(__name__)
-app.config["CORS_HEADERS"] = "Content-Type"
 
-cors = CORS(app, resources={r"/getimage": {"origins": "http://localhost:8001/"}})
+CORS(app)
+# cors = CORS(app, resources={r"/": {"origins": "http://localhost:8001/"}})
 
 
 @app.route("/", methods=["GET", "POST"])
-@cross_origin(origin="localhost", headers=["Content- Type", "Authorization"])
 def index():
     return "<h1> Deployed to Heroku</h1>"
 
 
 @app.route("/getimage", methods=["POST"])
-@cross_origin(origin="localhost", headers=["Content- Type", "Authorization"])
 def getimage():
     try:
+
         data = (request.data.decode()).split(",")[1]
         body = base64.decodebytes(data.encode("utf-8"))
         img = Image.open(BytesIO(body))
@@ -34,8 +33,20 @@ def getimage():
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
         scene_class = label_img_scene.classify(RGB_img)
+        # make_response = jsonify({"scene": scene_class})
+        # make_response.headers.add(
+        #     "Access-Control-Allow-Origin", "http://localhost:8001/"
+        # )
+        # make_response.headers.add(
+        #     "Access-Control-Allow-Headers", "Content-Type,Authorization"
+        # )
+        # make_response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+        # make_response.headers.add("Access-Control-Allow-Credentials", "true")
+        # print(make_response)
         return scene_class
-    except:
+
+    except Exception as e:
+        print(e)
         return "error"
 
 
